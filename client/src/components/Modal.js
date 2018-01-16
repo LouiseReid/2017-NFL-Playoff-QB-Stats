68641f2035dfbed45217871c99dcea6aba9eca1b
LouiseReid <l_reid@hotmail.co.uk>
2018-01-16 22:27:55 +0000

    modal styling refactored to css file

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,32 +1,16 @@
 import React from 'react'
+import '../stylesheets/modal.css';
+
 
 class Modal extends React.Component {
   render() {
     if (this.props.isOpen === false)
       return null
 
-    let modalStyle = {
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      zIndex: '9999',
-      background: '#fff'
-    }
-
-    let backdropStyle = {
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
-      top: '0px',
-      left: '0px',
-      zIndex: '9998',
-      background: 'rgba(0, 0, 0, 0.3)'
-    }
 
     return (
       <div>
-        <div style={modalStyle}>
+        <div className="modalStyle">
           <h2>{this.props.player.name} - {this.props.player.team}</h2>
           <li>Passing Attempts: {this.props.player.passingAttempts}</li>
           <li>Completions: {this.props.player.completions}</li>
@@ -36,7 +20,7 @@ class Modal extends React.Component {
           <li>First Down %: {this.props.player.firstDownPercentage}</li>
           <li>Sacked: {this.props.player.sacked}</li>
         </div>
-        <div style={backdropStyle} onClick={e => this.close(e)}/>
+        <div className="backdropStyle" onClick={e => this.close(e)}/>
       </div>
     )
   }
