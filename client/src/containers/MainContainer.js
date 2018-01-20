f7482cd88e0d062c9eb9e258c014141fde3723e8
LouiseReid <l_reid@hotmail.co.uk>
2018-01-20 14:00:27 +0000

    installed router

--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import PlayerList from '../components/PlayerList'
+import PlayerList from '../components/PlayerList';
+import NavBar from '../components/NavBar';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import '../stylesheets/main.css';
 
 
