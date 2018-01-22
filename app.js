beb384a4b595ead37628b995e2d7cb2230dab309
LouiseReid <l_reid@hotmail.co.uk>
2018-01-22 14:51:05 +0000

    folder tidy

--- app.js
+++ app.js
@@ -5,16 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
 var players = require('./routes/players');
 var playoffs = require('./routes/playoffs');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -22,7 +16,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
 app.use('/players', players);
 app.use('/playoffs', playoffs);
 
